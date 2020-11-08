c1285bc3b85590e96a7a1eaeb5ff1be16bd65f00
Parag More <paragmore306@gmail.com>
2020-11-08 12:05:36 +0530

    Initial Commit

--- client/src/components/Join/Join.js
+++ client/src/components/Join/Join.js
@@ -11,8 +11,8 @@ function Join() {
                 <h1 className="heading">
                     Join   
                 </h1>
-                <div><input placeholder="" className="joinInput" type="text" onChange={(e)=>{setName(e.target.value)}}></input></div>
-                <div><input placeholder="" className="joinInput mt-20" type="text" onChange={(e)=>{setRoom(e.target.value)}}></input></div>
+                <div><input placeholder="Type your Name" className="joinInput" type="text" onChange={(e)=>{setName(e.target.value)}}></input></div>
+                <div><input placeholder="Type the Room Name" className="joinInput mt-20" type="text" onChange={(e)=>{setRoom(e.target.value)}}></input></div>
                 <Link onClick={e=>(!name||! room)? e.preventDefault(): null} to={`/chat?name=${name}&room=${room}`}>
                     <button className="button mt-20" type="submit"> Sign In</button>
                 </Link>
